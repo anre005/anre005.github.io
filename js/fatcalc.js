8be80834d7967258fcc2729316fe2cc2470c81a0
Andreas Recktenwald <a.recktenwald@mx.uni-saarland.de>
2017-06-25 09:42:51 +0200

    included fettrechner, fixes

---
+++ js/fatcalc.js
@@ -0,0 +1,89 @@
+function fatcalc(){
+  var portion = $('#portion').val(),
+      fatact = $('#fatact').val()/100,
+      fatgoal = $('#fatgoal').val()/100,
+      fatfat = $('#fatfat').val()/100;
+
+  fatamount = (portion * (fatgoal - fatact)/(fatfat - fatact)).toFixed(2);
+
+  if(fatfat < fatact) {
+    document.getElementById("fatamounttext").innerHTML = `Bitte Angaben überprüfen. \n
+                                                          Der Fettanteil des Fleisches ist bereits höher als der des Fettes.`;
+    return ;
+  }
+
+  if(fatamount < 0) {
+    document.getElementById("fatamounttext").innerHTML = "Das Futter enthält bereits ausreichend Fett.";
+    return ;
+  }
+
+  if(fatgoal > fatact && fatgoal > fatfat) {
+    document.getElementById("fatamounttext").innerHTML = "Der gewünschte Fettanteil kann nicht erreicht werden.";
+    return ;
+  }
+
+  if(fatgoal == fatact) {
+    document.getElementById("fatamounttext").innerHTML = "Das Fleisch hat bereits den gewünschten Fett-Anteil.";
+    return ;
+  }
+  document.getElementById("fatamounttext").innerHTML = `Es sollten <strong>${fatamount}</strong> Gramm der Mahlzeit durch Fett ersetzt werden.`;
+
+  var bar_fat = AmCharts.makeChart("barfat",
+				{
+	"type": "serial",
+	"categoryField": "category",
+	"angle": 30,
+	"depth3D": 30,
+	"colors": [
+		"#2C79B0"
+	],
+	"startDuration": 1,
+	"startEffect": "bounce",
+	"fontSize": 12,
+	"thousandsSeparator": ".",
+	"export": {
+		"enabled": true
+	},
+	"categoryAxis": {
+		"gridPosition": "start"
+	},
+	"trendLines": [],
+	"graphs": [
+		{
+			"accessibleLabel": "[[category]] [[value]]",
+			"balloonText": "[[category]]:[[value]] Gramm",
+			"fillAlphas": 1,
+			"id": "AmGraph-1",
+			"title": "amgraph-1",
+			"type": "column",
+			"valueField": "column-1"
+		}
+	],
+	"guides": [],
+	"valueAxes": [
+		{
+			"id": "ValueAxis-1",
+			"stackType": "3d",
+			"title": "Menge in Gramm"
+		}
+	],
+	"allLabels": [],
+	"balloon": {},
+	"titles": [],
+	"dataProvider": [
+		{
+			"category": "Gesamtportion",
+			"column-1": portion
+		},
+		{
+			"category": "Muskelfleisch, Rohe fleischige Knochen, etc.",
+			"column-1": portion - fatamount
+		},
+		{
+			"category": "Fett",
+			"column-1": fatamount
+		}
+	]
+});
+		return bar_fat;
+}
--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
  //$(document).on("pagecreate", function(){
  // Do some processing when the button with id 'calc' is clicked or tapped
 
+var pie_food = null;
+
 function futtercalc() {
       //event.preventDefault();
         var weight = $('#weight').val(),
             amount = $('#amount').val(),
-            //zeitraum = $('input[name=radio-choice-range]:checked', '#futterwerte').val(),
             zeitraum_helper = document.getElementById("timerange"),
             zeitraum = zeitraum_helper.options[zeitraum_helper.selectedIndex].text,
-            //cat = $('input[name=radio-choice-pet]:checked', '#futterwerte').val(),
             cat_helper = document.getElementById("petchoice"),
             cat = cat_helper.options[cat_helper.selectedIndex].text,
             anname = $('#anname').val(),
@@ -35,26 +35,114 @@ function futtercalc() {
       if(zeitraum == 'Woche') leber = 0.05 * amount/100 * weight * 7;
       if(zeitraum == 'Monat') leber = 0.05 * amount/100 * weight * 30;
 
+      unit_fleisch = "Kilogramm";
+      unit_rfk = "Kilogramm";
+      unit_inn = "Kilogramm";
+
       fleisch = fleisch.toFixed(2);
-      knochen = knochen.toFixed(2);
-      leber = leber.toFixed(2);
+      if(fleisch < 1) {
+        fleisch_tab = fleisch * 1000;
+        unit_fleisch = "Gramm";
+      } else {
+        fleisch_tab = fleisch;
+      }
+      knochen = knochen.toFixed(4);
+      if(knochen < 1) {
+        knochen_tab = knochen * 1000;
+        unit_rfk = "Gramm";
+      } else {
+        knochen_tab = knochen;
+      }
+      leber = leber.toFixed(4);
+      if(leber < 1) {
+        leber_tab = leber * 1000;
+        unit_inn = "Gramm";
+      } else {
+        leber_tab = leber;
+      }
 
-      /*$('#fleischout').text(fleisch);
-      $('#knochenout').text(knochen);
-      $('#leberout').text(leber);
-      $('#innereienout').text(leber);
-      $('#zeitraumout').text(zeitraum);
-      $('#annameout').text(anname);*/
 
-      document.getElementById("petname").innerHTML = `Futtermengen für ${anname}`;
+      document.getElementById("petname").innerHTML = `Futtermengen für ${anname} pro ${zeitraum}`;
 
-      document.getElementById("amountlist").innerHTML = `<ul>
-              <li>Fütterungsmenge pro: <strong>${zeitraum}</strong></li>
-              <li>Fleisch: <strong>${fleisch} kg</strong></li>
-              <li>Knochen: <strong>${knochen} kg</strong> (keine blanken Knochen füttern)</li>
-              <li>Leber: <strong>${leber} kg</strong></li>
-              <li>Andere Innereien (Milz, Nieren, ...): <strong>${leber} kg</strong></li>
-            </ul>`;
+      document.getElementById("amountlist").innerHTML = `
+              <table class="table table-responsive table-striped">
+                <thead>
+                  <th>Muskelfleisch</th>
+                  <th>Rohe fleischige Knochen</th>
+                  <th>Leber</th>
+                  <th>Andere Innereien (Leber, Milz, ...)</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>${fleisch_tab} ${unit_fleisch}</td>
+                    <td>${knochen_tab} ${unit_rfk}</td>
+                    <td>${leber_tab} ${unit_inn}</td>
+                    <td>${leber_tab} ${unit_inn}</td>
+                  </tr>
+                </tbody>
+              </table>
+                `;
+
+      /*var pie_food = AmCharts.makeChart("foodamount",
+				{
+					"type": "pie",
+					"balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]] Kilogramm </b> ([[percents]]%)</span>",
+					"titleField": "type",
+					"valueField": "kilo",
+					"fontSize": 12,
+					"theme": "default",
+					"allLabels": [],
+					"balloon": {},
+					"titles": [],
+          "marginTop": 0,
+					"dataProvider": [
+						{
+							"type": "Muskelfleisch",
+							"kilo": fleisch
+						},
+						{
+							"type": "Rohe fleischige Knochen",
+							"kilo": knochen
+						},
+						{
+							"type": "Leber",
+							"kilo": leber
+						},
+						{
+							"type": "Milz, Niere, etc.",
+							"kilo": leber
+						}
+					]
+				}
+			);*/
+
+      /*if(pie_food !== null) {
+        pie_food.destroy();
+      }*/
+
+      var food_data = [
+        {"label": "Fleisch",
+				"value": parseFloat(fleisch),
+				"color": "#2383c1"},
+				{
+				"label": "Rohe fleischige Knochen",
+				"value": parseFloat(knochen),
+				"color": "#64a61f"
+			  },
+			  {
+				"label": "Leber",
+				"value": parseFloat(leber),
+				"color": "#7b6788"
+			},
+			{
+				"label": "Andere Innereien",
+				"value": parseFloat(leber),
+				"color": "#a05c56"
+			}
+    ];
+
+    //pie_food.updateProp("data.content", food_data);
+  //return pie_food;
 }
 //});
 
@@ -68,3 +156,108 @@ function test1() {
     alert('Wert unbekannt');
   }
 }
+
+function pie_destroy() {
+  if(pie_food !== null) {
+          pie_food.redraw();
+  }
+}
+
+/*var food_data_init = [
+        {"label": "Fleisch",
+				"value": 10 * 0.8 * 0.03,
+				"color": "#2383c1"},
+				{
+				"label": "Rohe fleischige Knochen",
+				"value": 10 * 0.1 * 0.03,
+				"color": "#64a61f"
+			  },
+			  {
+				"label": "Leber",
+				"value": 10 * 0.05 * 0.03,
+				"color": "#7b6788"
+			},
+			{
+				"label": "Andere Innereien",
+				"value": 10 * 0.05 * 0.03,
+				"color": "#a05c56"
+			}
+    ];
+
+var pie_food = new d3pie("foodamount", {
+	"header": {
+		"title": {
+			"fontSize": 24,
+			"font": "open sans"
+		},
+		"subtitle": {
+			"color": "#999999",
+			"fontSize": 12,
+			"font": "open sans"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 10,
+		"font": "open sans",
+		"location": "bottom-left"
+	},
+	"size": {
+		"canvasHeight": 400,
+		"canvasWidth": 600,
+		"pieInnerRadius": "71%",
+		"pieOuterRadius": "74%"
+	},
+	"data": {
+		"sortOrder": "value-desc",
+		"content": food_data_init
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 32
+		},
+		"mainLabel": {
+			"fontSize": 11
+		},
+		"percentage": {
+			"color": "#ffffff",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#adadad",
+			"fontSize": 11
+		},
+		"lines": {
+			"enabled": true
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"tooltips": {
+		"enabled": true,
+		"type": "placeholder",
+		"string": "{label}: {value}, {percentage}%"
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "back",
+			"speed": 400,
+			"size": 20
+		}
+	},
+	"misc": {
+		"gradient": {
+			"enabled": true,
+			"percentage": 100
+		},
+		"canvasPadding": {
+			"top": 0,
+			"right": 0,
+			"bottom": 0,
+			"left": 0
+		}
+	},
+	"callbacks": {}
+});*/
